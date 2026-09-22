d3b3a29c6b3348c8d32d95f00300aed82119f687
junminahn <36021827+junminahn@users.noreply.github.com>
2026-09-22 05:53:02 +0000

    Deploying to pages-angular from @ egose/shadcn-theme@1002f9eb522bda30e95139dbe89e0201902f76c5 🚀

---
+++ chunk-B20SLP5J2.js
@@ -0,0 +1,1 @@
+import{z as Ji}from"./chunk-BkE_jlx-.js";import{n as et}from"./chunk-DNmirxej.js";var r=class t{constructor(){et(()=>`tw:bg-muted tw:rounded-md tw:block tw:motion-safe:animate-pulse`)}static ɵfac=function(s){return new(s||t)};static ɵdir=Ji({type:t,selectors:[[``,`hlmSkeleton`,``],[`hlm-skeleton`]],hostAttrs:[`data-slot`,`skeleton`]})};export{r as t};
